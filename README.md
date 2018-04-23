5c9a878fd2bdba96463d3230d097b582e57d2fbc
cms <chenmaosen0@gmail.com>
2018-04-23 16:42:11 +0800

    日常更新

--- README.md
+++
@@ -1,7 +0,0 @@
-# ML
-Implement frequent used machine learning algorithms
-
-# 注意
-  在积累中形成自己的 (Code Base) & (**Think** Base)
-  
-  
--- bash/免密登陆.sh
+++
@@ -1,9 +0,0 @@
-# 简易版本, 有时还需修改权限
-
-for ((i=19; i<=34; i++))
-do
-	scp ~/.ssh/id_rsa.pub sla${i}:~/
-	ssh sla${i} "mkdir ~/.ssh; cat ~/id_rsa.pub >> ~/.ssh/authorized_keys ; rm ~/id_rsa.pub"
-done
-
-
